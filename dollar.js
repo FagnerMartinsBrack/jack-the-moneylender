a23a18dd4b29fe962d864bd72401b2d5bf7bea41
Fagner Brack <github@fagnermartins.com>
2018-10-10 18:13:31 +1100

    :red_circle: :white_check_mark: :hammer: Create a 'Money' model to work around floating point issues

---
+++ dollar.js
@@ -0,0 +1,48 @@
+window.Money = (input) => {
+  const toDecimalPoint = (cents) => {
+    const sign = cents < 0 ? '-' : '';
+    const columns = cents < 0
+      ? cents.toString().split('').slice(1, Infinity)
+      : cents.toString().split('');
+    const result = columns.slice();
+    const dotLocation = columns.length - 2;
+
+    if (dotLocation < 0) {
+      return sign + '$0.0' + result.join('');
+    }
+    if (dotLocation === 0) {
+      return sign + '$0.' + result.join('');
+    }
+    if (dotLocation > 0) {
+      result.splice(dotLocation, 0, '.');
+      return sign + '$' + result.join('');
+    }
+  };
+
+  const thisCents = Number(
+    input
+      .split('$').join('')
+      .split('.').join('')
+  );
+
+  return {
+    cents: thisCents,
+    toString: () => input,
+    multipliedBy: (dollarValue) => {
+      const multiplicationInCents = thisCents * dollarValue.cents;
+      const resultOfMultiplication = Number(
+        multiplicationInCents
+          .toString()
+          .slice(0, -2)
+      );
+      return Money(toDecimalPoint(resultOfMultiplication));
+    },
+    minus: (dollarValue) => {
+      const resultOfSubtraction = thisCents - dollarValue.cents;
+      return Money(toDecimalPoint(resultOfSubtraction));
+    },
+    greaterThan: (dollarValue) => {
+      return thisCents > dollarValue.cents;
+    }
+  };
+};
